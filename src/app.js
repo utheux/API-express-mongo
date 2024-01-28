b7c2c3dd2de66ce968a1b32772225856f7290f6c
Matheus <utheuz_ribeiro@hotmail.com>
2024-01-28 17:00:50 -0300

    primeiro commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexao", erro);
+});
+
+conexao.once("open", ()=>{
+    console.log("Conexao com o banco feita com sucesso");
+});
+
+const app = express();
+routes(app);
+
+
+
+
+
+
+app.delete("/livros/:id", (req,res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("livro removido com sucesso");
+});
+
+
+
+export default app;
+
+// mongodb+srv://admin:<password>@cluster0.4caitxg.mongodb.net/?retryWrites=true&w=majority
---
+++ src/routes/autoresRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import AutorController from "../controllerr/autorController.js";
+
+const routes = express.Router();
+
+routes.get("/autores", AutorController.listarAutores);
+routes.get("/autores/:id", AutorController.listarAutorPorId);
+routes.post("/autores", AutorController.cadastrarAutor);
+routes.put("/autores/:id", AutorController.atualizarAutor);
+routes.delete("/autores/:id", AutorController.excluirAutor);
+export default routes;
